6dafb981cdb932840b8766759b9579569e6f5f94
Amar <amar.babuta@veolia.com.au>
2021-12-23 11:11:24 +1100

    mapper update

--- Map.py
+++ Map.py
@@ -334,103 +334,106 @@ class Map:
                                   legend_name=legend_name,
                                   show=False).add_to(self.map)
 
-    # Adding a legend method
-    def add_categorical_legend(self, title=None):
-        if title == None:
-            title = input('Enter title for legend:')
-        else:
-            title = title
-        legend_categories = ""
-        for label, color in self.dic_colors.items():
-            legend_categories += f"<li><span style='background:{color}'></span>{label}</li>"
 
-        legend_html = f"""
-        <div id='maplegend' class='maplegend'>
-          <div class='legend-title'>{title}</div>
-          <div class='legend-scale'>
-            <ul class='legend-labels'>
-            {legend_categories}
-            </ul>
-          </div>
-        </div>
-        """
-        script = f"""
-            <script type="text/javascript">
-            var oneTimeExecution = (function() {{
-                        var executed = false;
-                        return function() {{
-                            if (!executed) {{
-                                 var checkExist = setInterval(function() {{
-                                           if ((document.getElementsByClassName('leaflet-top leaflet-right').length) || (!executed)) {{
-                                              document.getElementsByClassName('leaflet-top leaflet-right')[0].style.display = "flex"
-                                              document.getElementsByClassName('leaflet-top leaflet-right')[0].style.flexDirection = "column"
-                                              document.getElementsByClassName('leaflet-top leaflet-right')[0].innerHTML += `{legend_html}`;
-                                              clearInterval(checkExist);
-                                              executed = true;
-                                           }}
-                                        }}, 100);
-                            }}
-                        }};
-                    }})();
-            oneTimeExecution()
-            </script>
-          """
+    # # Adding a legend method
+    # def add_categorical_legend(self, title=None):
 
-
-        css = """
-
-        <style type='text/css'>
-          .maplegend {
-            z-index:9999;
-            float:right;
-            background-color: rgba(255, 255, 255, 1);
-            border-radius: 5px;
-            border: 2px solid #bbb;
-            padding: 10px;
-            font-size:12px;
-            positon: relative;
-          }
-          .maplegend .legend-title {
-            text-align: left;
-            margin-bottom: 5px;
-            font-weight: bold;
-            font-size: 90%;
-            }
-          .maplegend .legend-scale ul {
-            margin: 0;
-            margin-bottom: 5px;
-            padding: 0;
-            float: left;
-            list-style: none;
-            }
-          .maplegend .legend-scale ul li {
-            font-size: 80%;
-            list-style: none;
-            margin-left: 0;
-            line-height: 18px;
-            margin-bottom: 2px;
-            }
-          .maplegend ul.legend-labels li span {
-            display: block;
-            float: left;
-            height: 16px;
-            width: 30px;
-            margin-right: 5px;
-            margin-left: 0;
-            border: 0px solid #ccc;
-            }
-          .maplegend .legend-source {
-            font-size: 80%;
-            color: #777;
-            clear: both;
-            }
-          .maplegend a {
-            color: #777;
-            }
-        </style>
-        """
-
-        self.map.get_root().header.add_child(folium.Element(script + css))
+    # def add_categorical_legend(self, title=None):
+    #     if title == None:
+    #         title = input('Enter title for legend:')
+    #     else:
+    #         title = title
+    #     legend_categories = ""
+    #     for label, color in self.dic_colors.items():
+    #         legend_categories += f"<li><span style='background:{color}'></span>{label}</li>"
+    #
+    #     legend_html = f"""
+    #     <div id='maplegend' class='maplegend'>
+    #       <div class='legend-title'>{title}</div>
+    #       <div class='legend-scale'>
+    #         <ul class='legend-labels'>
+    #         {legend_categories}
+    #         </ul>
+    #       </div>
+    #     </div>
+    #     """
+    #     script = f"""
+    #         <script type="text/javascript">
+    #         var oneTimeExecution = (function() {{
+    #                     var executed = false;
+    #                     return function() {{
+    #                         if (!executed) {{
+    #                              var checkExist = setInterval(function() {{
+    #                                        if ((document.getElementsByClassName('leaflet-top leaflet-right').length) || (!executed)) {{
+    #                                           document.getElementsByClassName('leaflet-top leaflet-right')[0].style.display = "flex"
+    #                                           document.getElementsByClassName('leaflet-top leaflet-right')[0].style.flexDirection = "column"
+    #                                           document.getElementsByClassName('leaflet-top leaflet-right')[0].innerHTML += `{legend_html}`;
+    #                                           clearInterval(checkExist);
+    #                                           executed = true;
+    #                                        }}
+    #                                     }}, 100);
+    #                         }}
+    #                     }};
+    #                 }})();
+    #         oneTimeExecution()
+    #         </script>
+    #       """
+    #
+    #
+    #     css = """
+    #
+    #     <style type='text/css'>
+    #       .maplegend {
+    #         z-index:9999;
+    #         float:right;
+    #         background-color: rgba(255, 255, 255, 1);
+    #         border-radius: 5px;
+    #         border: 2px solid #bbb;
+    #         padding: 10px;
+    #         font-size:12px;
+    #         positon: relative;
+    #       }
+    #       .maplegend .legend-title {
+    #         text-align: left;
+    #         margin-bottom: 5px;
+    #         font-weight: bold;
+    #         font-size: 90%;
+    #         }
+    #       .maplegend .legend-scale ul {
+    #         margin: 0;
+    #         margin-bottom: 5px;
+    #         padding: 0;
+    #         float: left;
+    #         list-style: none;
+    #         }
+    #       .maplegend .legend-scale ul li {
+    #         font-size: 80%;
+    #         list-style: none;
+    #         margin-left: 0;
+    #         line-height: 18px;
+    #         margin-bottom: 2px;
+    #         }
+    #       .maplegend ul.legend-labels li span {
+    #         display: block;
+    #         float: left;
+    #         height: 16px;
+    #         width: 30px;
+    #         margin-right: 5px;
+    #         margin-left: 0;
+    #         border: 0px solid #ccc;
+    #         }
+    #       .maplegend .legend-source {
+    #         font-size: 80%;
+    #         color: #777;
+    #         clear: both;
+    #         }
+    #       .maplegend a {
+    #         color: #777;
+    #         }
+    #     </style>
+    #     """
+    #
+    #     self.map.get_root().header.add_child(folium.Element(script + css))
 
     def show_or_save(self, option_show=False, option_save=False, map_name=None):
         """
