044fdcce42df056f61ac47d7e6b5ac9e3e5a6144
Amar <amar.babuta@veolia.com.au>
2022-01-11 10:35:10 +1100

    categorical legend

--- Map.py
+++ Map.py
@@ -1,5 +1,5 @@
 # IMPORTS
-
+from branca.element import Template, MacroElement
 import folium
 import matplotlib.pyplot as plt
 import matplotlib
@@ -346,93 +346,98 @@ class Map:
         for label, color in self.dic_colors.items():
             legend_categories += f"<li><span style='background:{color}'></span>{label}</li>"
 
-        legend_html = f"""
-        <div id='maplegend' class='maplegend'>
-          <div class='legend-title'>{title}</div>
-          <div class='legend-scale'>
-            <ul class='legend-labels'>
-            {legend_categories:}
-            </ul>
-          </div>
-        </div>
-        """
-        script = f"""
-            <script type="text/javascript">
-            var oneTimeExecution = (function() {{
-                        var executed = false;
-                        return function() {{
-                            if (!executed) {{
-                                 var checkExist = setInterval(function() {{
-                                           if ((document.getElementsByClassName('leaflet-top leaflet-right').length) || (!executed)) {{
-                                              document.getElementsByClassName('leaflet-top leaflet-right')[0].style.display = "flex"
-                                              document.getElementsByClassName('leaflet-top leaflet-right')[0].style.flexDirection = "column"
-                                              document.getElementsByClassName('leaflet-top leaflet-right')[0].innerHTML += `{legend_html}`;
-                                              clearInterval(checkExist);
-                                              executed = true;
-                                           }}
-                                        }}, 100);
-                            }}
-                        }};
-                    }})();
-            oneTimeExecution()
+        template = """
+        {% macro html(this, kwargs) %}
+
+        <!doctype html>
+        <html lang="en">
+        <head>
+            <meta charset="utf-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1">
+            <title>jQuery UI Draggable - Default functionality</title>
+            <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
+
+            <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
+            <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+
+            <script>
+            $( function() {
+                $( "#maplegend" ).draggable({
+                                start: function (event, ui) {
+                                    $(this).css({
+                                        right: "auto",
+                                        top: "auto",
+                                        bottom: "auto"
+                                    });
+                                }
+                            });
+        });
+
             </script>
-          """
+        </head>
+        <body>
 
 
-        css = """
+        <div id='maplegend' class='maplegend'
+            style='position: absolute; z-index:9999; border:2px solid grey; background-color:rgba(255, 255, 255, 0.8);
+            border-radius:6px; padding: 10px; font-size:14px; right: 20px; bottom: 20px;'>
+
+        <div class='legend-title'>Legend (draggable!)</div>
+        <div class='legend-scale'>
+            <ul class='legend-labels'>
+                {legend_categories:}
+            </ul>
+        </div>
+        </div>
+        </body>
+        </html>
 
         <style type='text/css'>
-          .maplegend {
-            z-index:9999;
-            float:right;
-            background-color: rgba(255, 255, 255, 1);
-            border-radius: 5px;
-            border: 2px solid #bbb;
-            padding: 10px;
-            font-size:12px;
-            positon: relative;
-          }
-          .maplegend .legend-title {
-            text-align: left;
-            margin-bottom: 5px;
-            font-weight: bold;
-            font-size: 90%;
-            }
-          .maplegend .legend-scale ul {
-            margin: 0;
-            margin-bottom: 5px;
-            padding: 0;
-            float: left;
-            list-style: none;
-            }
-          .maplegend .legend-scale ul li {
-            font-size: 80%;
-            list-style: none;
-            margin-left: 0;
-            line-height: 18px;
-            margin-bottom: 2px;
-            }
-          .maplegend ul.legend-labels li span {
-            display: block;
-            float: left;
-            height: 16px;
-            width: 30px;
-            margin-right: 5px;
-            margin-left: 0;
-            border: 0px solid #ccc;
-            }
-          .maplegend .legend-source {
-            font-size: 80%;
-            color: #777;
-            clear: both;
-            }
-          .maplegend a {
-            color: #777;
-            }
+            .maplegend .legend-title {
+                text-align: left;
+                margin-bottom: 5px;
+                font-weight: bold;
+                font-size: 90%;
+                }
+            .maplegend .legend-scale ul {
+                margin: 0;
+                margin-bottom: 5px;
+                padding: 0;
+                float: left;
+                list-style: none;
+                }
+            .maplegend .legend-scale ul li {
+                font-size: 80%;
+                list-style: none;
+                margin-left: 0;
+                line-height: 18px;
+                margin-bottom: 2px;
+                }
+            .maplegend ul.legend-labels li span {
+                display: block;
+                float: left;
+                height: 16px;
+                width: 30px;
+                margin-right: 5px;
+                margin-left: 0;
+                border: 1px solid #999;
+                }
+            .maplegend .legend-source {
+                font-size: 80%;
+                color: #777;
+                clear: both;
+                }
+            .maplegend a {
+                color: #777;
+                }
         </style>
-        """
+        {% endmacro %}"""
 
-        self.map.get_root().header.add_child(folium.Element(script + css))
+        macro = MacroElement()
+        macro._template = Template(template)
+
+        self.map.get_root().add_child(macro)
+
 
     def show_or_save(self, option_show=False, option_save=False, map_name=None):
         """
