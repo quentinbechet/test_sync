562cced1d65c86656abaf9776052f83478c8ee93
Amar <amar.babuta@veolia.com.au>
2022-01-11 14:56:27 +1100

    cat

--- Map.py
+++ Map.py
@@ -1,5 +1,5 @@
 # IMPORTS
-
+from branca.element import Template, MacroElement
 import folium
 import matplotlib.pyplot as plt
 import matplotlib
@@ -346,40 +346,55 @@ class Map:
         for label, color in self.dic_colors.items():
             legend_categories += f"<li><span style='background:{color}'></span>{label}</li>"
 
-        legend_html = f"""
-        <div id='maplegend' class='maplegend'>
-          <div class='legend-title'>{title}</div>
-          <div class='legend-scale'>
+        template = """
+        {% macro html(this, kwargs) %}
+
+        <!doctype html>
+        <html lang="en">
+        <head>
+            <meta charset="utf-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1">
+            <title>jQuery UI Draggable - Default functionality</title>
+            <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
+
+            <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
+            <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+
+            <script>
+            $( function() {
+                $( "#maplegend" ).draggable({
+                                start: function (event, ui) {
+                                    $(this).css({
+                                        right: "auto",
+                                        top: "auto",
+                                        bottom: "auto"
+                                    });
+                                }
+                            });
+        });
+
+        </script>
+        </head>
+        <body>
+
+
+        <div id='maplegend' class='maplegend'
+            style='position: absolute; z-index:9999; border:2px solid grey; background-color:rgba(255, 255, 255, 0.8);
+            border-radius:6px; padding: 10px; font-size:14px; right: 20px; bottom: 20px;'>
+
+        <div class='legend-title'>Legend (draggable!)</div>
+        <div class='legend-scale'>
             <ul class='legend-labels'>
-            {legend_categories}
+                <li><span style='background:red;opacity:0.7;'></span>Big</li>
+                <li><span style='background:orange;opacity:0.7;'></span>Medium</li>
+                <li><span style='background:green;opacity:0.7;'></span>Small</li>
+
             </ul>
-          </div>
         </div>
-        """
-        script = f"""
-            <script type="text/javascript">
-            var oneTimeExecution = (function() {{
-                        var executed = false;
-                        return function() {{
-                            if (!executed) {{
-                                 var checkExist = setInterval(function() {{
-                                           if ((document.getElementsByClassName('leaflet-top leaflet-right').length) || (!executed)) {{
-                                              document.getElementsByClassName('leaflet-top leaflet-right')[0].style.display = "flex"
-                                              document.getElementsByClassName('leaflet-top leaflet-right')[0].style.flexDirection = "column"
-                                              document.getElementsByClassName('leaflet-top leaflet-right')[0].innerHTML += `{legend_html}`;
-                                              clearInterval(checkExist);
-                                              executed = true;
-                                           }}
-                                        }}, 100);
-                            }}
-                        }};
-                    }})();
-            oneTimeExecution()
-            </script>
-          """
+        </div>
 
-
-        css = """
+        </body>
+        </html>
 
         <style type='text/css'>
             .maplegend .legend-title {
@@ -420,9 +435,13 @@ class Map:
                 color: #777;
                 }
         </style>
-        """
+        {% endmacro %}"""
 
-        self.map.get_root().header.add_child(folium.Element(script + css))
+        macro = MacroElement()
+        macro._template = Template(template)
+
+        self.map.get_root().add_child(macro)
+
 
     def show_or_save(self, option_show=False, option_save=False, map_name=None):
         """
